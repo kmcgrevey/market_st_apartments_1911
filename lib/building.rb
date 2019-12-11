18d4523ce59de5ae128cbc835e64862686920857
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-11 10:25:36 -0700

    Complete iteration 2

--- lib/building.rb
+++ lib/building.rb
@@ -1,3 +1,21 @@
 class Building
+  attr_accessor :units
 
+  def initialize
+    @units = []
+  end
+
+  def add_unit(unit)
+    @units << unit
+  end
+
+  def average_rent
+    @units.count
+    rents = []
+    @units.find_all do |unit|
+      # require "pry"; binding.pry
+      rents << unit.monthly_rent
+    end
+    rents.sum.to_f / @units.count
+  end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -6,9 +6,32 @@ require_relative '../lib/building'
 
 class BuildingTest < Minitest::Test
 
+  def setup
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+  end
   def test_it_exists
     building = Building.new
     assert_instance_of Building, building
+  end
+
+  def test_it_has_no_units
+    building = Building.new
+    assert_equal [], building.units
+  end
+
+  def test_it_can_add_units
+    building = Building.new
+    building.add_unit(@unit1)
+    building.add_unit(@unit2)
+    assert_equal [@unit1, @unit2], building.units
+  end
+
+  def test_it_can_calculate_average_rent
+    building = Building.new
+    building.add_unit(@unit1)
+    building.add_unit(@unit2)
+    assert_equal 1099.5, building.average_rent
   end
 
 end
