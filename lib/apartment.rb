0c3497c745e36c43267beb2606dfc417575065e0
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-11 09:17:02 -0700

    Initial commit

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,1 +1,12 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  
+  def initialize(unit)
+    @unit_number = unit[:number]
+    @monthly_rent = unit[:monthly_rent]
+    @bathrooms = unit[:bathrooms]
+    @bedrooms = unit[:bedrooms]
+  end
 
+
+end
